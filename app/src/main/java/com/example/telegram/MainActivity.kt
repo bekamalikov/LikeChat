ad5b0b9279ad907c1010543a75dbc239e405b15a
beknurbai <beknurbaihan@gmail.com>
2020-10-03 09:19:05 +0600

    1)запршивать разрешения у пользователя 2)вывел запрос на другую корутину 3)считываю контакты

--- app/src/main/java/com/example/telegram/MainActivity.kt
+++ app/src/main/java/com/example/telegram/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.telegram
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.example.telegram.activities.RegisterActivity
 import com.example.telegram.databinding.ActivityMainBinding
 import com.example.telegram.models.User
@@ -13,6 +15,9 @@ import com.example.telegram.ui.fragments.ChatsFragment
 import com.example.telegram.ui.objects.AppDrawer
 import com.example.telegram.utilits.*
 import com.theartofdev.edmodo.cropper.CropImage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +32,9 @@ class MainActivity : AppCompatActivity() {
         APP_ACTIVITY = this
         initFirebase()
         initUser {
+            CoroutineScope(Dispatchers.IO).launch {
+                initContact()
+            }
             initFields()
             initFunc()
         }
@@ -61,5 +69,20 @@ class MainActivity : AppCompatActivity() {
         AppStates.updateStates(AppStates.OFFLINE)
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //запрос разрешения
+        if (ContextCompat.checkSelfPermission(
+                APP_ACTIVITY,
+                READ_CONTACTS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            initContact()
+        }
+    }
 
 }
--- app/src/main/java/com/example/telegram/utilits/appDataBaseHelper.kt
+++ app/src/main/java/com/example/telegram/utilits/appDataBaseHelper.kt
@@ -1,6 +1,9 @@
 package com.example.telegram.utilits
 
 import android.net.Uri
+import android.provider.ContactsContract
+import androidx.core.content.contentValuesOf
+import com.example.telegram.models.CommonModel
 import com.example.telegram.models.User
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
@@ -16,7 +19,6 @@ lateinit var USER: User
 
 const val NODE_USERS = "users"
 const val NODE_USERNAMES = "usernames"
-
 const val FOLDER_PROFILE_PHOTO = "profile_image"
 const val CHILD_ID = "id"
 const val CHILD_PHONE = "phone"
@@ -24,9 +26,7 @@ const val CHILD_USERNAME = "username"
 const val CHILD_FULLNAME = "fullname"
 const val CHILD_BIO = "bio"
 const val CHILD_PHOTO_URL = "photoURL"
-const val CHILD_STATE="state"
-
-
+const val CHILD_STATE = "state"
 
 
 fun initFirebase() {
@@ -38,6 +38,7 @@ fun initFirebase() {
 }
 
 inline fun putUrlToDataBase(url: String, crossinline function: () -> Unit) {
+    //фунция высшего порядка отправляет url image v fb
     REF_DATABASE_ROOT.child(NODE_USERS).child(CURRENT_UID)
         .child(CHILD_PHOTO_URL).setValue(url)
         .addOnSuccessListener { function() }
@@ -47,6 +48,7 @@ inline fun putUrlToDataBase(url: String, crossinline function: () -> Unit) {
 }
 
 inline fun getUrlFromStorage(path: StorageReference, crossinline function: (url: String) -> Unit) {
+    //функиця высшего порядка ,достает картинку с хранилища по url
     path.downloadUrl
         .addOnSuccessListener { function(it.toString()) }
         .addOnFailureListener {
@@ -55,6 +57,7 @@ inline fun getUrlFromStorage(path: StorageReference, crossinline function: (url:
 }
 
 inline fun putImageToStorage(uri: Uri, path: StorageReference, crossinline function: () -> Unit) {
+    //функция высшего порядка ,отправляет картинку в хранилище
     path.putFile(uri)
         .addOnSuccessListener { function() }
         .addOnFailureListener {
@@ -63,6 +66,7 @@ inline fun putImageToStorage(uri: Uri, path: StorageReference, crossinline funct
 }
 
 inline fun initUser(crossinline function: () -> Unit) {
+    //функция высшего порядка ,инициализация текущей модели юсер
     REF_DATABASE_ROOT.child(NODE_USERS).child(CURRENT_UID)
         .addListenerForSingleValueEvent(AppValueEventListener {
             USER = it.getValue(User::class.java) ?: User()
@@ -71,4 +75,33 @@ inline fun initUser(crossinline function: () -> Unit) {
             }
             function()
         })
+}
+//функция для считаывания контактов
+fun initContact() {
+    if (checkPermission(READ_CONTACTS)) {
+        var arrayContacts = arrayListOf<CommonModel>()
+        val cursor = APP_ACTIVITY.contentResolver.query(
+            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+        cursor?.let {
+            while (it.moveToNext()) {
+                val fullName =
+                    it.getString(it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                val phone =
+                    it.getString(it.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                val newModel = CommonModel()
+                newModel.fullname = fullName // присвоение модельке фулнейма
+                newModel.phone = phone.replace(
+                    Regex("[\\s,-]"),
+                    ""
+                )//присвоение номера телефона меделке и удаление пробела и тире с номера
+                arrayContacts.add(newModel)
+            }
+        }
+        cursor?.close()
+    }
 }
